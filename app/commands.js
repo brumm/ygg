c7175e928298509a089d07255bf760a9bb231abd
Philipp Brumm <philipp.brumm@gmail.com>
2021-11-21 11:08:37 +0100

    center window horizontally on mouse display after resizing

--- app/commands.js
+++ app/commands.js
@@ -1,4 +1,4 @@
-const { ipcMain, BrowserWindow } = require('electron')
+const { ipcMain, BrowserWindow, screen } = require('electron')
 
 ipcMain.handle('resize', async (event, { width, height }) => {
   const [window] = BrowserWindow.getAllWindows()
@@ -8,9 +8,20 @@ ipcMain.handle('resize', async (event, { width, height }) => {
   height = height || currentHeight
 
   window.setSize(width, height)
+  centerWindowHorizontallyOnDisplayWithCursor(window)
 })
 
 ipcMain.handle('hide', async (event) => {
   const [window] = BrowserWindow.getAllWindows()
   window.hide()
 })
+
+const centerWindowHorizontallyOnDisplayWithCursor = (win) => {
+  const point = screen.getCursorScreenPoint()
+  const display = screen.getDisplayNearestPoint(point)
+  const bounds = win.getBounds()
+
+  const x = display.bounds.x + display.size.width / 2 - bounds.width / 2
+  const y = bounds.y
+  win.setPosition(x, y)
+}
