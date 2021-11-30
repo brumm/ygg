047db9fbcdc3bbd103800347008d28ab46d3b317
Philipp Brumm <philipp.brumm@gmail.com>
2021-11-30 16:47:38 +0100

    ok ok

---
+++ server/core.js
@@ -0,0 +1,216 @@
+import { readFile } from 'fs/promises'
+
+import sift from 'sift'
+
+import { itemToId } from './utils.js'
+import path from 'path'
+
+const debug = false
+const log = debug ? console.log : () => {}
+
+const itemCatalogItem = {
+  id: 'item-catalog',
+  types: ['item-catalog'],
+}
+
+const actionCatalogItem = {
+  id: 'action-catalog',
+  types: ['action-catalog'],
+}
+
+class Ygg {
+  cache = {}
+  providers = []
+  actions = []
+  iconCache = {}
+
+  constructor({ plugins }) {
+    const context = {
+      cache: this.cache,
+      providers: this.providers,
+      actions: this.actions,
+    }
+
+    for (const plugin of plugins) {
+      if (plugin.providers) {
+        for (const provider of plugin.providers) {
+          if (typeof provider === 'function') {
+            this.providers.push(provider(context))
+          } else {
+            this.providers.push(provider)
+          }
+        }
+      }
+
+      if (plugin.actions) {
+        for (const action of plugin.actions) {
+          if (typeof action === 'function') {
+            this.actions.push(action(context))
+          } else {
+            this.actions.push(action)
+          }
+        }
+      }
+    }
+  }
+
+  async initialize() {
+    this.cache.core = [itemCatalogItem, actionCatalogItem]
+
+    try {
+      console.time('prime cache')
+      for (const item of this.cache.core) {
+        await this.getChildrenForItem(item.id)
+      }
+    } catch (error) {
+      console.error('error initializing ygg', error)
+    } finally {
+      console.timeEnd('prime cache')
+    }
+  }
+
+  async getChildrenForItem(parentItemId) {
+    const parentItem = this.getItemById(parentItemId)
+
+    const matchingProviders = this.providers.filter(
+      sift({ inputTypes: { $in: parentItem.types } }),
+    )
+
+    let children = []
+    for (const provider of matchingProviders) {
+      const items = await this.runProvider(provider, parentItem.meta, this)
+      children = children.concat(items)
+    }
+
+    for (const item of children) {
+      item.id = item.id || itemToId(item)
+      item.parentId = parentItem.id
+      item.hasChildren = this.providers.some(
+        sift({ inputTypes: { $in: item.types } }),
+      )
+    }
+
+    return children
+  }
+
+  async getActionsForItem(itemId) {
+    const item = this.getItemById(itemId)
+
+    const matchingProviders = this.providers.filter(
+      sift({
+        inputTypes: { $in: actionCatalogItem.types },
+      }),
+    )
+
+    let actions = []
+    for (const provider of matchingProviders) {
+      const items = await this.runProvider(provider, actionCatalogItem.meta)
+      actions = actions.concat(items)
+    }
+
+    for (const item of actions) {
+      item.id = item.id || itemToId(item)
+      item.parentId = actionCatalogItem.id
+      item.hasChildren = this.providers.some(
+        sift({ inputTypes: { $in: item.types } }),
+      )
+    }
+
+    actions = actions.filter(
+      sift({
+        types: { $in: ['action'] },
+        directTypes: { $in: item.types },
+      }),
+    )
+
+    return actions
+  }
+
+  async getIndirectsForAction(itemId, actionItemId) {
+    const item = this.getItemById(itemId)
+    const actionItem = this.getItemById(actionItemId)
+
+    if (!actionItem.indirectTypes) {
+      return null
+    }
+
+    const items = await this.getChildrenForItem(item)
+    return items.filter(sift({ types: { $in: actionItem.indirectTypes } }))
+  }
+
+  async runProvider(provider, meta) {
+    const cacheKey = JSON.stringify([provider.id, meta])
+
+    if (this.cache[cacheKey]) {
+      log('[cache] cache hit', cacheKey)
+      return this.cache[cacheKey]
+    }
+
+    log('[cache] cache miss', cacheKey)
+    const items = await provider.run(meta)
+    this.cache[cacheKey] = items
+    return items
+  }
+
+  async runAction(directItemId, actionItemId, indirectItemId) {
+    const directItem = this.getItemById(directItemId)
+    const actionItem = this.getItemById(actionItemId)
+    const indirectItem = this.getItemById(indirectItemId)
+
+    console.log({ directItem, actionItem, indirectItem })
+
+    return actionItem.run(directItem, indirectItem)
+  }
+
+  getItemById(id) {
+    const allItems = Object.values(this.cache).flat()
+    return allItems.find((item) => item.id === id)
+  }
+
+  async getIconForItem(itemId) {
+    const item = this.getItemById(itemId)
+    const cacheKey = item.id
+
+    if (this.iconCache[cacheKey]) {
+      log('[cache] cache hit', cacheKey)
+      return this.iconCache[cacheKey]
+    }
+
+    const provider = this.providers.find(
+      sift({ outputTypes: { $in: item.types } }),
+    )
+
+    if (!provider) {
+      const iconBuffer = await readFile(
+        path.join(
+          import.meta.url.replace('file:', ''),
+          '../images/missing.png',
+        ),
+      )
+      return iconBuffer
+    }
+
+    try {
+      if (provider.getIcon) {
+        const iconBuffer = await provider.getIcon(item.meta)
+        log('[cache] cache miss', cacheKey)
+        this.iconCache[cacheKey] = iconBuffer
+
+        return iconBuffer
+      }
+
+      const iconBuffer = await readFile(
+        path.join(
+          import.meta.url.replace('file:', ''),
+          '../images/missing.png',
+        ),
+      )
+      return iconBuffer
+    } catch (error) {
+      console.error({ error, provider, item, self: this })
+      return error
+    }
+  }
+}
+
+export default Ygg
--- server/index.js
+++ server/index.js
@@ -4,17 +4,7 @@ import createFastify from 'fastify'
 import cors from 'fastify-cors'
 import fastifyHttpErrorsEnhanced from 'fastify-http-errors-enhanced'
 
-import {
-  getActionsForItem,
-  getItemById,
-  getChildrenForItem,
-  getIndirectsForAction,
-  getIconForItem,
-  getIconForAction,
-  getActionById,
-  initialize,
-  runProvider,
-} from './lib.js'
+import Ygg from './core.js'
 
 import * as fileSystemPlugin from './plugins/file-system'
 import * as corePlugin from './plugins/core.js'
@@ -29,66 +19,27 @@ fastify.register(fastifyHttpErrorsEnhanced, { hideUnhandledErrors: false })
 
 const plugins = [fileSystemPlugin, corePlugin, plainTextPlugin]
 
-const context = (global.context = {})
+const ygg = new Ygg({ plugins })
+global.ygg = ygg
 
-const cache = {}
-const providers = []
-const actions = []
-
-for (const plugin of plugins) {
-  if (plugin.providers) {
-    for (const provider of plugin.providers) {
-      if (typeof provider === 'function') {
-        providers.push(provider(context))
-      } else {
-        providers.push(provider)
-      }
-    }
-  }
-
-  if (plugin.actions) {
-    for (const action of plugin.actions) {
-      if (typeof action === 'function') {
-        actions.push(action(context))
-      } else {
-        actions.push(action)
-      }
-    }
-  }
-}
-
-context.providers = providers
-context.actions = actions
-context.cache = cache
-
-initialize(context)
-
-try {
-  console.time('prime cache')
-  for (const item of context.cache.core) {
-    getChildrenForItem(item, context)
-  }
-  console.timeEnd('prime cache')
-} catch {}
+await ygg.initialize()
 
 // --- item
 
 fastify.get('/items/:itemId', async (request, reply) => {
-  return getItemById(request.params.itemId, context)
+  return ygg.getItemById(request.params.itemId)
 })
 
 // --- direct
 
 fastify.get('/items/:itemId/children', async (request, reply) => {
-  const item = getItemById(request.params.itemId, context)
-  return getChildrenForItem(item, context)
+  return ygg.getChildrenForItem(request.params.itemId)
 })
 
 // --- action
 
 fastify.get('/items/:itemId/actions', async (request, reply) => {
-  const item = getItemById(request.params.itemId, context)
-  return getActionsForItem(item, context)
+  return ygg.getActionsForItem(request.params.itemId)
 })
 
 // --- indirect
@@ -96,27 +47,23 @@ fastify.get('/items/:itemId/actions', async (request, reply) => {
 fastify.get(
   '/items/default/actions/:actionId/indirects',
   async (request, reply) => {
-    const action = getActionById(request.params.actionId, context)
-
     if (!action.indirectTypes) {
       return []
     }
 
-    return getIndirectsForAction(null, action, context)
+    return ygg.getIndirectsForAction(null, request.params.actionId)
   },
 )
 
 fastify.get(
   '/items/:itemId/actions/:actionId/indirects',
   async (request, reply) => {
-    const item = getItemById(request.params.itemId, context)
-    const action = getActionById(request.params.actionId, context)
-
-    if (!action.indirectTypes) {
-      return []
-    }
-
-    return getIndirectsForAction(item || null, action, context)
+    return (
+      ygg.getIndirectsForAction(
+        request.params.itemId || null,
+        request.params.actionId,
+      ) ?? []
+    )
   },
 )
 
@@ -124,25 +71,17 @@ fastify.get(
 
 fastify.post('/execute-command', async (request, reply) => {
   const { directId, actionId, indirectId } = JSON.parse(request.body)
-  const direct = getItemById(directId, context)
-  const action = getActionById(actionId, context)
-  const indirect = getItemById(indirectId, context)
-
-  action.run(direct, indirect)
-
-  return null
+  return ygg.runAction(directId, actionId, indirectId)
 })
 
 // --- icons
 
 fastify.get('/items/:itemId/icon', async (request, reply) => {
-  const item = getItemById(request.params.itemId, context)
-  return getIconForItem(item, context)
+  return ygg.getIconForItem(request.params.itemId)
 })
 
 fastify.get('/actions/:actionId/icon', async (request, reply) => {
-  const action = getActionById(request.params.actionId, context)
-  return getIconForAction(action, context)
+  return ygg.getIconForItem(request.params.actionId)
 })
 
 // ---
